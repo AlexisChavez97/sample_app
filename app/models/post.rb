ca3be9ec70f7dfbc4b18fec8f462e66f6353680d
Alexis Chavez <achavez@michelada.io>
2018-03-07 17:39:43 -0600

    resolved active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :picture
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, length: { maximum: 140 }
-  validates :picture, presence: true, if: Proc.new { |post| post.content.blank? }
-  validates :content, presence: true, if: Proc.new { |post| post.picture.blank? }
-  validate  :picture_size
-
-  private
-
-  def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, 'should be less than 5MB')
-    end
-  end
 end
 
